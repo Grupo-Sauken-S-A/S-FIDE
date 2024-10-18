a11a78d6d3ff2b3e7382c7fdadf8a76633534ab4
jirios125 <jir.main.business@gmail.com>
2024-10-18 15:40:39 -0300

    feat: Token Certificate Extractor done

--- src/main/java/com/sauken/ds_cod/TokenCertificateExtractor.java
+++ src/main/java/com/sauken/ds_cod/TokenCertificateExtractor.java
@@ -9,29 +9,29 @@ import java.security.Security;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.Enumeration;
 
 public class TokenCertificateExtractor {
 
     public static void main(String[] args) {
-        if (args.length != 4) {
-            System.out.println("Uso: java -jar token-cert-extractor.jar <ruta del driver PKCS#11> <contraseña del token> <número de slot> <alias del certificado>");
+        if (args.length != 3) {
+            System.out.println("Uso: java -jar token-cert-extractor.jar <ruta del driver PKCS#11> <contraseña del token> <número de slot>");
             System.exit(1);
         }
 
         String pkcs11LibraryPath = args[0];
         String password = args[1];
         int slotNumber = Integer.parseInt(args[2]);
-        String certificateAlias = args[3];
 
         try {
-            extractCertificate(pkcs11LibraryPath, password, slotNumber, certificateAlias);
+            extractCertificate(pkcs11LibraryPath, password, slotNumber);
         } catch (Exception e) {
             System.err.println("Error al leer el token: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    private static void extractCertificate(String pkcs11LibraryPath, String password, int slotNumber, String certificateAlias)
+    private static void extractCertificate(String pkcs11LibraryPath, String password, int slotNumber)
             throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
         // Configurar el proveedor PKCS#11
         String config = String.format(
@@ -50,12 +50,14 @@ public class TokenCertificateExtractor {
         KeyStore keyStore = KeyStore.getInstance("PKCS11");
         keyStore.load(null, password.toCharArray());
 
-        // Extraer el certificado especificado
-        if (keyStore.containsAlias(certificateAlias)) {
-            Certificate cert = keyStore.getCertificate(certificateAlias);
+        // Buscar el certificado en el slot
+        Enumeration<String> aliases = keyStore.aliases();
+        if (aliases.hasMoreElements()) {
+            String alias = aliases.nextElement();
+            Certificate cert = keyStore.getCertificate(alias);
             if (cert instanceof X509Certificate) {
                 X509Certificate x509Cert = (X509Certificate) cert;
-                System.out.println("Información del Certificado:");
+                System.out.println("Información del Certificado en el slot " + slotNumber + ":");
                 System.out.println("Sujeto: " + x509Cert.getSubjectX500Principal());
                 System.out.println("Emisor: " + x509Cert.getIssuerX500Principal());
                 System.out.println("Número de Serie: " + x509Cert.getSerialNumber());
@@ -63,10 +65,10 @@ public class TokenCertificateExtractor {
                 System.out.println("Válido hasta: " + x509Cert.getNotAfter());
                 System.out.println("Algoritmo de Firma: " + x509Cert.getSigAlgName());
             } else {
-                System.out.println("El certificado no es de tipo X.509");
+                System.out.println("El certificado en el slot " + slotNumber + " no es de tipo X.509");
             }
         } else {
-            System.out.println("No se encontró un certificado con el alias especificado: " + certificateAlias);
+            System.out.println("No se encontró ningún certificado en el slot " + slotNumber);
         }
     }
 }
